01504a30b072681aa5d210d61127adb75906860e
psg9790 <81291990+psg9790@users.noreply.github.com>
2024-01-04 16:07:01 +0900

    next

--- PersonExample/Assets/Scripts/TAGO_WebRequest.cs
+++ PersonExample/Assets/Scripts/TAGO_WebRequest.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 using System;
+using UnityEngine.UI;
 
 public class TAGO_WebRequest : MonoBehaviour
 {
     [SerializeField] GameObject cellPrefab;
     [SerializeField] Transform content;
+
+    [SerializeField] Text pageDisplayText;
+    private int pageIndex = 1;
+    private int minIndex = 1;
+    private int maxIndex = 5;
 
     private void Start()
     {
@@ -16,7 +22,12 @@ public class TAGO_WebRequest : MonoBehaviour
 
     IEnumerator TAGO_ShipListRequest()
     {
-        UnityWebRequest www = UnityWebRequest.Get("http://apis.data.go.kr/1613000/DmstcShipNvgInfoService/getShipOpratInfoList?serviceKey=3t8heetsrzhDJSU1UfM8V6q6pH28VXzklod%2FbQMtbHbS8UijuhH%2BPiywBIzME1nG2xAFXxiMLDY3NQtmqo1NZQ%3D%3D&depPlandTime=20230403&depNodeId=SEA96140&_type=json");
+        UnityWebRequest www = UnityWebRequest.Get($"http://apis.data.go.kr/1613000/DmstcShipNvgInfoService/getShipOpratInfoList?" +
+        $"serviceKey=3t8heetsrzhDJSU1UfM8V6q6pH28VXzklod%2FbQMtbHbS8UijuhH%2BPiywBIzME1nG2xAFXxiMLDY3NQtmqo1NZQ%3D%3D&" +
+        $"depPlandTime=20230403&" +
+        $"depNodeId=SEA96140&" +
+        $"_type=json&" +
+        $"pageNo={pageIndex}");
 
         // 전송완료 후 값이 올때까지 기다림
         yield return www.SendWebRequest();
@@ -25,6 +36,9 @@ public class TAGO_WebRequest : MonoBehaviour
         {
             Container container = JsonUtility.FromJson<Container>(www.downloadHandler.text);
 
+            pageDisplayText.text = $"page {container.response.body.pageNo} / {Mathf.Ceil(container.response.body.totalCount / container.response.body.numOfRows)}";
+            maxIndex = (int)Mathf.Ceil(container.response.body.totalCount / container.response.body.numOfRows);
+
             foreach (var item in container.response.body.items.item)
             {
                 GameObject go = Instantiate(cellPrefab, content);
@@ -32,6 +46,34 @@ public class TAGO_WebRequest : MonoBehaviour
                 cell.SetItem(item);
             }
         }
+    }
+
+    public void NextButtonClick()
+    {
+        ClearContent();
+        AddPageIndex(+1);
+        StartCoroutine(TAGO_ShipListRequest());
+    }
+
+    public void PrevButtonClick()
+    {
+        ClearContent();
+        AddPageIndex(-1);
+        StartCoroutine(TAGO_ShipListRequest());
+    }
+
+    void ClearContent()
+    {
+        Transform[] childs = content.GetComponentsInChildren<Transform>();
+
+        for (int i = 1; i < childs.Length; i++)
+            Destroy(childs[i].gameObject);
+    }
+
+    void AddPageIndex(int amount)
+    {
+        pageIndex += amount;
+        pageIndex = Mathf.Clamp(pageIndex, 1, 5);
     }
 }
 
